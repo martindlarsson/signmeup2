d8e571c5d20fcf3e0404d2903a064d6bfaf9eb98
martindlarsson <martin.d.andersson@gmail.com>
2016-01-29 11:32:01 +0100

    Missade att checka in två filer

---
+++ SignMeUp2/ViewModels/RegistreringVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SignMeUp2.ViewModels
+{   
+    public class RegistreringVM
+    {
+        public RegistreringVM()
+        {
+            Svar = new List<FaltSvarVM>();
+        }
+
+        public int Id { get; set; }
+
+        public int? FormularId { get; set; }
+        public virtual FormularViewModel Formular { get; set; }
+
+        public virtual ICollection<FaltSvarVM> Svar { get; set; }
+        
+        public bool HarBetalt { get; set; }
+
+        public string Kommentar { get; set; }
+
+        public string PaysonToken { get; set; }
+        
+        public DateTime? Registreringstid { get; set; }
+
+        public int AttBetala { get; set; }
+
+        public int Forseningsavgift { get; set; }
+
+        public int Rabatt { get; set; }
+        public string Rabattkod { get; set; }
+
+        public InvoiceViewModel Invoice { get; set; }
+    }
+}
